3423f1e69c699fdb980a6d7c23248705bd287cce
buisnesskey <amr.alsayd.w88@gmail.com>
2019-08-29 18:11:14 +0200

    --- Auto-Git Commit ---

--- database/migrations/2014_10_12_000002_create_technicians_table.php
+++ database/migrations/2014_10_12_000002_create_technicians_table.php
@@ -15,31 +15,23 @@ class CreateTechniciansTable extends Migration
     {
         Schema::create('technicians', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->nullable($value = true);
-            $table->string('email')->nullable($value = true);
-            $table->string('password')->nullable($value = true);
-            $table->string('code')->nullable($value = true);
-            $table->string('mobile')->nullable($value = true);
-            $table->text('address')->nullable($value = true);
-            $table->string('lat')->nullable($value = true);
-            $table->string('lng')->nullable($value = true);
-            $table->string('image')->nullable($value = true);
-            $table->string('device_token')->nullable($value = true);
-            $table->enum('role', ['admin','user','fannie']);    
-            $table->string('status');        
-            $table->tinyInteger('type')->nullable($value = true);       
-            $table->string('lang')->nullable($value = 'ar'); 
-                  
+            
+            $table->string('renewal_date')->nullable($value = true);
+            $table->string('available')->nullable($value = true);
+            $table->text('brief')->nullable($value = true);
             $table->unsignedInteger('user_id')->nullable($value = true);
             $table->unsignedInteger('service_id')->nullable($value = true);
             $table->unsignedInteger('country_id')->nullable($value = true);
             $table->unsignedInteger('city_id')->nullable($value = true);
             $table->unsignedInteger('area_id')->nullable($value = true);
+            $table->unsignedInteger('nationality_id ')->nullable($value = true);
+
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null'); 
             $table->foreign('service_id')->references('id')->on('services')->onDelete('set null'); 
             $table->foreign('country_id')->references('id')->on('countries')->onDelete('set null'); 
             $table->foreign('city_id')->references('id')->on('cities')->onDelete('set null'); 
             $table->foreign('area_id')->references('id')->on('areas')->onDelete('set null'); 
+            $table->foreign('nationality_id')->references('id')->on('nationalities')->onDelete('set null'); 
             
             $table->rememberToken();
             $table->timestamps();
@@ -53,6 +45,6 @@ class CreateTechniciansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('technicians');
     }
 }
