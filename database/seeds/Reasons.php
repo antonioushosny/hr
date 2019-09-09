44cc916733059f11fe6887fa97d8fb02be3a0bf1
buisnesskey <amr.alsayd.w88@gmail.com>
2019-09-09 11:11:21 +0200

    reasons seeder permission

---
+++ database/seeds/Reasons.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class Reasons extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $permissions = [
+
+           'reasons_list',
+           'reasons_create',
+           'reasons_edit',
+           'reasons_delete',
+    
+        ];
+        
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+    }
+}
